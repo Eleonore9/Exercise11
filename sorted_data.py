209b9a53962b326e963c30c048a57f3e38bebc27
ccontrast <ericachang018@gmail.com>
2012-10-09 17:23:54 -0700

    last version

--- sorted_data.py
+++ sorted_data.py
@@ -1,2 +1,33 @@
-#This is our Exercise11
-#Here is additional code
+#Open text file
+my_file = open("scores.txt")
+
+#Create initial dictionary
+my_dict = {}
+
+#Read the file into the dictionary
+for line in my_file:
+
+    split = line.strip().split(":")
+
+    my_dict[split[0]] = split[1]
+
+my_list = []
+
+#Make a list of restaurant names
+for restaurant, score in my_dict.items():
+    my_tup = (restaurant, score)
+    my_list.append(my_tup)
+
+#Sort the list of restaurant names
+sort_list = sorted(my_list)
+
+#Loop through the list of restaurant names and get the corresponding score
+for elements in sort_list:
+
+    restaurant = elements[0]
+    score = int(elements[1])
+
+    print "Restaurant %s is rated at %d." % (restaurant, score)
+
+#Close file
+my_file.close()
